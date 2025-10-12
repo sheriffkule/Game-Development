f8ba7438f459900b52eada627d03fe0b1b6410d8
SheriffKule <zeljko225@yahoo.com>
2025-10-12 19:59:32 +0200

    Fish Game 1

---
+++ 006 - Fish Game/script.js
@@ -0,0 +1,99 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 800;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
+
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  click: false,
+};
+
+canvas.addEventListener('mousedown', function (event) {
+  mouse.click = true;
+  mouse.x = event.x - canvasPosition.left;
+  mouse.y = event.y - canvasPosition.top;
+});
+
+canvas.addEventListener('mouseup', function () {
+  mouse.click = false;
+});
+
+class Player {
+  constructor() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.radius = 50;
+    this.angle = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.frame = 0;
+    this.spriteWidth = 498;
+    this.spriteHeight = 327;
+  }
+  update() {
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    if (mouse.x != this.x) {
+      this.x -= dx / 20;
+    }
+    if (mouse.y != this.y) {
+      this.y -= dy / 20;
+    }
+  }
+  draw() {
+    if (mouse.click) {
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+    ctx.fillStyle = 'purple';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.fillRect(this.x, this.y, this.radius, 10);
+  }
+}
+const player = new Player();
+
+const bubblesArray = [];
+class Bubble {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = 50;
+    this.speed = Math.random() * 5 + 1;
+    this.distance;
+  }
+  update() {
+    this.y -= this.speed;
+  }
+  draw() {
+    ctx.fillStyle = 'dodgerblue';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  player.draw();
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
