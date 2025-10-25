34d956647a3e3419db52c9d75a91f57ab6d684d0
SheriffKule <zeljko225@yahoo.com>
2025-10-26 01:11:33 +0200

    Flying Creatures 2

--- 010 - Flying Creatures/script.js
+++ 010 - Flying Creatures/script.js
@@ -4,36 +4,52 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 30;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'enemy1.png';
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image = new Image();
+    this.image.src = 'enemy4.png';
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 213;
+    this.spriteHeight = 212;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * (canvas.width);
+    this.newY = Math.random() * (canvas.height);
     this.frame = 0;
-    this.flapSpeed = Math.random() * 3 + 1;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    // this.angle = Math.random() * 500;
+    // this.angleSpeed = Math.random() * 1.5 + 0.5;
+    // this.curve = Math.random() * 200 + 50;
+    this.interval = Math.floor(Math.random() * 200 + 50);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
-    if (gameFrame % 2 === 0) {
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx / 70;
+    this.y -= dy / 70;
+
+    // this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 200) + (canvas.width / 2 - this.width / 2);
+    // this.y = canvas.height / 2 * Math.sin((this.angle * Math.PI) / 300) + (canvas.height / 2 - this.height / 2);
+    // this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
-      enemyImage,
+      this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
@@ -49,7 +65,7 @@ class Enemy {
 for (let i = 0; i < numberOfEnemies; i++) {
   enemiesArray.push(new Enemy());
 }
-console.log(enemiesArray);
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   enemiesArray.forEach((enemy) => {
