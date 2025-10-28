b37f00cab0c914b54556227f86b97ae85be8212a
SheriffKule <zeljko225@yahoo.com>
2025-10-28 22:41:03 +0100

    Creatures 2

--- 013 - Creatures/script.js
+++ 013 - Creatures/script.js
@@ -11,26 +11,33 @@ window.addEventListener('load', function () {
       this.width = width;
       this.height = height;
       this.enemies = [];
-      this.enemyInterval = 1000;
+      this.enemyInterval = 500;
       this.enemyTimer = 0;
+      this.enemyTypes = ['worm', 'ghost', 'spider'];
     }
     update(deltaTime) {
       this.enemies = this.enemies.filter((object) => !object.markedForDeletion);
       if (this.enemyTimer > this.enemyInterval) {
         this.#addNewEnemy();
         this.enemyTimer = 0;
-        console.log(this.enemies);
       } else {
         this.enemyTimer += deltaTime;
       }
 
-      this.enemies.forEach((object) => object.update());
+      this.enemies.forEach((object) => object.update(deltaTime));
     }
     draw() {
       this.enemies.forEach((object) => object.draw(this.ctx));
     }
     #addNewEnemy() {
+      const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
+      if (randomEnemy === 'worm') this.enemies.push(new Worm(this));
+      else if (randomEnemy === 'ghost') this.enemies.push(new Ghost(this));
+      else if (randomEnemy === 'spider') this.enemies.push(new Spider(this));
       this.enemies.push(new Worm(this));
+      //   this.enemies.sort(function (a, b) {
+      //     return a.y - b.y;
+      //   });
     }
   }
 
@@ -38,25 +45,107 @@ window.addEventListener('load', function () {
     constructor(game) {
       this.game = game;
       this.markedForDeletion = false;
+      this.frameX;
+      this.maxFrame = 5;
+      this.frameInterval = 100;
+      this.frameTimer = 0;
     }
-    update() {
-      this.x--;
+    update(deltaTime) {
+      this.x -= this.vx * deltaTime;
       if (this.x < 0 - this.width) this.markedForDeletion = true;
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX < this.maxFrame) this.frameX++;
+        else this.frameX = 0;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
     }
     draw(ctx) {
-      ctx.fillStyle = 'green';
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
     }
   }
 
   class Worm extends Enemy {
     constructor(game) {
       super(game);
+      this.spriteWidth = 229;
+      this.spriteHeight = 171;
+      this.width = this.spriteWidth / 2;
+      this.height = this.spriteHeight / 2;
       this.x = this.game.width;
-      this.y = Math.random() * this.game.height;
-      this.width = 200;
-      this.height = 100;
+      this.y = this.game.height - this.height;
       this.image = worm;
+      this.vx = Math.random() * 0.1 + 0.1;
+    }
+  }
+
+  class Ghost extends Enemy {
+    constructor(game) {
+      super(game);
+      this.spriteWidth = 261;
+      this.spriteHeight = 209;
+      this.width = this.spriteWidth / 2;
+      this.height = this.spriteHeight / 2;
+      this.x = this.game.width;
+      this.y = this.game.height * Math.random() * 0.6;
+      this.image = ghost;
+      this.vx = Math.random() * 0.2 + 0.1;
+      this.angle = 0;
+      this.curve = Math.random() * 3;
+    }
+    update(deltaTime) {
+      super.update(deltaTime);
+      this.y += Math.sin(this.angle) * this.curve;
+      this.angle += 0.05;
+    }
+    draw(ctx) {
+      ctx.save();
+      ctx.globalAlpha = 0.5;
+      super.draw(ctx);
+      ctx.restore();
+    }
+  }
+
+  class Spider extends Enemy {
+    constructor(game) {
+      super(game);
+      this.spriteWidth = 310;
+      this.spriteHeight = 175;
+      this.width = this.spriteWidth / 2;
+      this.height = this.spriteHeight / 2;
+      this.x = Math.random() * this.game.width;
+      this.y = 0 - this.height;
+      this.image = spider;
+      this.vx = 0;
+      this.vy = Math.random() * 0.1 + 0.1;
+      this.maxLength = Math.random() * game.height;
+      this.netWidth = Math.random() * 5 + 1
+    }
+    update(deltaTime) {
+      super.update(deltaTime);
+      if (this.y < 0 - this.height * 2) this.markedForDeletion = true;
+      this.y += this.vy * deltaTime;
+      if (this.y > this.maxLength) this.vy *= -1;
+    }
+    draw(ctx) {
+      ctx.strokeStyle = '#555';
+      ctx.lineWidth = this.netWidth;
+      ctx.beginPath();
+      ctx.moveTo(this.x + this.width / 2, 0);
+      ctx.lineTo(this.x + this.width / 2, this.y + this.height / 2);
+      ctx.stroke();
+      super.draw(ctx);
     }
   }
 
