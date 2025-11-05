ad8881b3f2cb428775bfe01776e2f6b8bd0646c0
SheriffKule <zeljko225@yahoo.com>
2025-11-05 17:15:05 +0100

    Forest Creatures Game 2

--- 018 - Forest Creatures Game/script.js
+++ 018 - Forest Creatures Game/script.js
@@ -26,18 +26,21 @@ window.addEventListener('load', function () {
       this.width = this.spriteWidth;
       this.height = this.spriteHeight;
       this.frameX = 0;
-      this.frameY = 3;
+      this.frameY = 4;
       this.maxFrame = 30;
       this.x = 500;
       this.y = 200;
       this.speedX = 0;
       this.speedY = 0;
-      this.maxSpeed = 10;
+      this.maxSpeed = 2;
       this.image = document.getElementById('owlbear');
+      this.fps = 60;
+      this.frameInterval = 1000 / this.fps;
+      this.frameTimer = 0;
     }
     draw(context) {
-      context.lineWidth = 8;
-      context.strokeRect(this.x, this.y, this.width, this.height);
+      //   context.lineWidth = 5;
+      //   context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(
         this.image,
         this.frameX * this.spriteWidth,
@@ -54,17 +57,31 @@ window.addEventListener('load', function () {
       this.speedX = speedX;
       this.speedY = speedY;
     }
-    update() {
+    update(deltaTime) {
       if (this.game.lastKey == 'P: ArrowLeft') {
         this.setSpeed(-this.maxSpeed, 0);
+        this.frameY = 3;
+      } else if (this.game.lastKey == 'R: ArrowLeft' && this.speedX < 0) {
+        this.setSpeed(0, 0);
+        this.frameY = 2;
       } else if (this.game.lastKey == 'P: ArrowRight') {
         this.setSpeed(this.maxSpeed, 0);
-      } else if (this.game.lastKey == 'P: ArrowUp') {
-        this.setSpeed(0, -this.maxSpeed);
-      } else if (this.game.lastKey == 'P: ArrowDown') {
-        this.setSpeed(0, this.maxSpeed);
-      } else {
+        this.frameY = 5;
+      } else if (this.game.lastKey == 'R: ArrowRight' && this.speedX > 0) {
         this.setSpeed(0, 0);
+        this.frameY = 4;
+      } else if (this.game.lastKey == 'P: ArrowUp') {
+        this.setSpeed(0, -this.maxSpeed * 0.6);
+        this.frameY = 7;
+      } else if (this.game.lastKey == 'R: ArrowUp' && this.speedY < 0) {
+        this.setSpeed(0, 0);
+        this.frameY = 6;
+      } else if (this.game.lastKey == 'P: ArrowDown') {
+        this.setSpeed(0, this.maxSpeed * 0.6);
+        this.frameY = 1;
+      } else if (this.game.lastKey == 'R: ArrowDown' && this.speedY > 0) {
+        this.setSpeed(0, 0);
+        this.frameY = 0;
       }
       this.x += this.speedX;
       this.y += this.speedY;
@@ -81,16 +98,70 @@ window.addEventListener('load', function () {
         this.y = this.game.height - this.height;
       }
       // sprite animation
-      if (this.frameX < this.maxFrame) {
-        this.frameX++;
+      if (this.frameTimer > this.frameInterval) {
+        this.frameX < this.maxFrame ? this.frameX++ : (this.frameX = 0);
+        // if (this.frameX < this.maxFrame) {
+        //   this.frameX++;
+        // } else {
+        //   this.frameX = 0;
+        // }
+        this.frameTimer = 0;
       } else {
-        this.frameX = 0;
+        this.frameTimer += deltaTime;
       }
     }
   }
 
   class Object {
-    constructor() {}
+    constructor(game) {
+      this.game = game;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    update() {}
+  }
+
+  class Bush extends Object {
+    constructor(game) {
+      super(game);
+      this.game = game;
+      this.image = document.getElementById('bush');
+      this.imageWidth = 216;
+      this.imageHeight = 100;
+      this.width = this.imageWidth;
+      this.height = this.imageHeight;
+      this.x = Math.random() * this.game.width - this.width;
+      this.y = this.game.topMargin + Math.random() * (this.game.height - this.game.topMargin - this.height);
+    }
+  }
+
+  class Plant extends Object {
+    constructor(game) {
+      super(game);
+      this.game = game;
+      this.image = document.getElementById('plant');
+      this.imageWidth = 212;
+      this.imageHeight = 118;
+      this.width = this.imageWidth;
+      this.height = this.imageHeight;
+      this.x = Math.random() * this.game.width - this.width;
+      this.y = this.game.topMargin + Math.random() * (this.game.height - this.game.topMargin - this.height);
+    }
+  }
+
+  class Grass extends Object {
+    constructor(game) {
+      super(game);
+      this.game = game;
+      this.image = document.getElementById('grass');
+      this.imageWidth = 103;
+      this.imageHeight = 182;
+      this.width = this.imageWidth;
+      this.height = this.imageHeight;
+      this.x = Math.random() * this.game.width - this.width;
+      this.y = this.game.topMargin + Math.random() * (this.game.height - this.game.topMargin - this.height);
+    }
   }
 
   class Game {
@@ -101,20 +172,41 @@ window.addEventListener('load', function () {
       this.lastKey = undefined;
       this.input = new InputHandler(this);
       this.owlBear = new OwlBear(this);
+      this.numberOfPlants = 10;
+      this.plants = [];
+      this.gameObjects = [];
     }
-    render(context) {
-      this.owlBear.draw(context);
-      this.owlBear.update();
+    render(context, deltaTime) {
+      this.gameObjects = [...this.plants, this.owlBear];
+      this.gameObjects.sort((a, b) => {
+        return a.y + a.height - (b.y + b.height);
+      });
+      this.gameObjects.forEach((object) => {
+        object.draw(context);
+        object.update(deltaTime);
+      });
+    }
+    init() {
+      for (let i = 0; i < this.numberOfPlants; i++) {
+        const randomize = Math.random();
+
+        if (randomize < 0.3) this.plants.push(new Bush(this));
+        else if (randomize < 0.6) this.plants.push(new Grass(this));
+        else this.plants.push(new Plant(this));
+      }
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  game.render(ctx);
+  let lastTime = 0;
+  game.init();
 
-  function animate() {
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.render(ctx);
+    game.render(ctx, deltaTime);
     requestAnimationFrame(animate);
   }
-  animate();
+  animate(0);
 });
