448d285df1e816ffc02e0c5cfdcd415eaf80ad91
SheriffKule <zeljko225@yahoo.com>
2025-11-07 14:20:22 +0100

    Object Pool / Performance Optimization 2

--- 020 - Object Pool Optimization/script.js
+++ 020 - Object Pool Optimization/script.js
@@ -11,27 +11,51 @@ window.addEventListener('load', function () {
     constructor(game) {
       this.game = game;
       this.radius = 75;
-      this.x = Math.random() * this.game.width;
+      this.x = -this.radius;
       this.y = Math.random() * this.game.height;
       this.image = document.getElementById('asteroid');
       this.spriteWidth = 150;
       this.spriteHeight = 155;
-      this.speed = 1;
+      this.speed = Math.random() * 1.5 + 0.1;
+      this.free = true;
+      this.angle = 0;
+      this.va = Math.random() * 0.02 - 0.01;
     }
     draw(context) {
-      context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-      context.stroke();
-      context.drawImage(
-        this.image,
-        this.x - this.spriteWidth * 0.5,
-        this.y - this.spriteHeight * 0.5,
-        this.spriteWidth,
-        this.spriteHeight
-      );
+      if (!this.free) {
+        // context.beginPath();
+        // context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        // context.stroke();
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.drawImage(
+          this.image,
+          -this.spriteWidth * 0.5,
+          -this.spriteHeight * 0.5,
+          this.spriteWidth,
+          this.spriteHeight
+        );
+        context.restore();
+      }
     }
     update() {
+      this.angle += this.va;
+      this.y += Math.cos(this.angle) * 0.5;
+      if (!this.free) {
         this.x += this.speed;
+        if (this.x > this.game.width + this.radius) {
+          this.reset();
+        }
+      }
+    }
+    reset() {
+      this.free = true;
+    }
+    start() {
+      this.free = false;
+      this.x = -this.radius;
+      this.y = Math.random() * this.game.height;
     }
   }
 
@@ -40,7 +64,9 @@ window.addEventListener('load', function () {
       this.width = width;
       this.height = height;
       this.asteroidPool = [];
-      this.max = 3;
+      this.max = 30;
+      this.asteroidTimer = 0;
+      this.asteroidInterval = 1000;
       this.createAsteroidPool();
     }
     createAsteroidPool() {
@@ -48,17 +74,38 @@ window.addEventListener('load', function () {
         this.asteroidPool.push(new Asteroid(this));
       }
     }
-    render(context) {
+    getElement() {
+      for (let i = 0; i < this.asteroidPool.length; i++) {
+        if (this.asteroidPool[i].free) {
+          return this.asteroidPool[i];
+        }
+      }
+    }
+    render(context, deltaTime) {
+      // create asteroids periodically
+      if (this.asteroidTimer > this.asteroidInterval) {
+        const asteroid = this.getElement();
+        if (asteroid) asteroid.start();
+        this.asteroidTimer = 0;
+      } else {
+        this.asteroidTimer += deltaTime;
+      }
       this.asteroidPool.forEach((asteroid) => {
         asteroid.draw(context);
-        asteroid.update()
+        asteroid.update();
       });
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  game.render(ctx);
+  let lastTime = 0;
 
-  function animate() {}
-  animate();
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx, deltaTime);
+    requestAnimationFrame(animate);
+  }
+  animate(0);
 });
