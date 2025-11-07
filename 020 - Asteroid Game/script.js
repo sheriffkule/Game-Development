a51b024ec74da0d52f7caee0f3c4f0acc2dfbaf7
SheriffKule <zeljko225@yahoo.com>
2025-11-07 15:58:46 +0100

    Object Pool / Performance Optimization 3

--- 020 - Asteroid Game/script.js
+++ 020 - Asteroid Game/script.js
@@ -40,9 +40,9 @@ window.addEventListener('load', function () {
       }
     }
     update() {
-      this.angle += this.va;
-      this.y += Math.cos(this.angle) * 0.5;
       if (!this.free) {
+        this.angle += this.va;
+        this.y += Math.cos(this.angle) * 0.5;
         this.x += this.speed;
         if (this.x > this.game.width + this.radius) {
           this.reset();
@@ -59,32 +59,115 @@ window.addEventListener('load', function () {
     }
   }
 
+  class Explosion {
+    constructor(game) {
+      this.game = game;
+      this.x = 0;
+      this.y = 0;
+      this.speed = 0;
+      this.image = document.getElementById('explosions');
+      this.spriteWidth = 300;
+      this.spriteHeight = 300;
+      this.free = true;
+      this.frameX = 0;
+      this.frameY = Math.floor(Math.random() * 3);
+      this.maxFrame = 22;
+      this.animationTimer = 0;
+      this.animationInterval = 1000 / 22;
+    }
+    draw(context) {
+      if (!this.free) {
+        context.drawImage(
+          this.image,
+          this.frameX * this.spriteWidth,
+          this.frameY * this.spriteHeight,
+          this.spriteWidth,
+          this.spriteHeight,
+          this.x - this.spriteWidth * 0.5,
+          this.y - this.spriteHeight * 0.5,
+          this.spriteWidth,
+          this.spriteHeight
+        );
+      }
+    }
+    update(deltaTime) {
+      if (!this.free) {
+        if (this.animationTimer > this.animationInterval) {
+          this.frameX++;
+          if (this.frameX > this.maxFrame) this.reset();
+          this.animationTimer = 0;
+        } else {
+          this.animationTimer += deltaTime;
+        }
+      }
+    }
+    reset() {
+      this.free = true;
+      this.frameY = Math.floor(Math.random() * 3);
+    }
+    start(x, y) {
+      this.free = false;
+      this.x = x;
+      this.y = y;
+      this.frameX = 0;
+    }
+  }
+
   class Game {
     constructor(width, height) {
       this.width = width;
       this.height = height;
       this.asteroidPool = [];
-      this.max = 30;
+      this.maxAsteroids = 30;
       this.asteroidTimer = 0;
       this.asteroidInterval = 1000;
       this.createAsteroidPool();
+
+      this.mouse = {
+        x: 0,
+        y: 0,
+      };
+
+      this.explosionPool = [];
+      this.maxExplosions = 20
+      this.createExplosionPool();
+
+      window.addEventListener('click', (e) => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        const explosion = this.getExplosion();
+        if (explosion) explosion.start(this.mouse.x, this.mouse.y);
+        console.log(explosion);
+      });
     }
     createAsteroidPool() {
-      for (let i = 0; i < this.max; i++) {
+      for (let i = 0; i < this.maxAsteroids; i++) {
         this.asteroidPool.push(new Asteroid(this));
       }
     }
-    getElement() {
+    createExplosionPool() {
+      for (let i = 0; i < this.maxExplosions; i++) {
+        this.explosionPool.push(new Explosion(this));
+      }
+    }
+    getAsteroid() {
       for (let i = 0; i < this.asteroidPool.length; i++) {
         if (this.asteroidPool[i].free) {
           return this.asteroidPool[i];
         }
       }
     }
+    getExplosion() {
+      for (let i = 0; i < this.explosionPool.length; i++) {
+        if (this.explosionPool[i].free) {
+          return this.explosionPool[i];
+        }
+      }
+    }
     render(context, deltaTime) {
       // create asteroids periodically
       if (this.asteroidTimer > this.asteroidInterval) {
-        const asteroid = this.getElement();
+        const asteroid = this.getAsteroid();
         if (asteroid) asteroid.start();
         this.asteroidTimer = 0;
       } else {
@@ -93,6 +176,10 @@ window.addEventListener('load', function () {
       this.asteroidPool.forEach((asteroid) => {
         asteroid.draw(context);
         asteroid.update();
+      });
+      this.explosionPool.forEach((explosion) => {
+        explosion.draw(context);
+        explosion.update(deltaTime);
       });
     }
   }
