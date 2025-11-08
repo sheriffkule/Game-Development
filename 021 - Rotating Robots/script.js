d685560c44400860d1d7a5f0d6304f237f5d4b3b
SheriffKule <zeljko225@yahoo.com>
2025-11-08 14:42:12 +0100

    Rotating Robots 1

--- 021 - Rotating Robots/script.js
+++ 021 - Rotating Robots/script.js
@@ -6,19 +6,32 @@ window.addEventListener('load', function () {
   canvas.height = 800;
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 3;
-  ctx.fillStyle = 'white';
 
   class Robot {
     constructor(canvas) {
       this.canvas = canvas;
       this.x = this.canvas.width * 0.5;
       this.y = this.canvas.height * 0.5;
+      this.centerX = this.x;
+      this.centerY = this.y;
       this.radius = 80;
       this.angle = 0;
+      this.spriteWidth = 370;
+      this.spriteHeight = 393;
+      this.frameX = 0;
+      this.maxFrame = 75;
       this.bodyImage = document.getElementById('body');
+      this.bodySprite = document.getElementById('bodySprite');
       this.eye1Image = document.getElementById('eye1');
       this.eye2Image = document.getElementById('eye2');
-      this.reflection = document.getElementById('reflection');
+      this.reflectionImage = document.getElementById('reflectionImage');
+      this.detectorLight = document.getElementById('detectorLight');
+      this.eye1Radius = this.radius * 0.4;
+      this.eye2Radius = this.radius * 0.65;
+      this.eye1Distance = this.eye1Radius;
+      this.eye2Distance = this.eye2Radius;
+      this.tracking = false;
+      this.movementAngle = 0;
       this.mouse = {
         x: 0,
         y: 0,
@@ -26,40 +39,78 @@ window.addEventListener('load', function () {
       this.canvas.addEventListener('mousemove', (e) => {
         this.mouse.x = e.offsetX;
         this.mouse.y = e.offsetY;
+        this.tracking = true;
+      });
+      this.canvas.addEventListener('mouseleave', (e) => {
+        this.tracking = false;
       });
     }
     draw(context) {
       // body
       context.drawImage(
-        this.bodyImage,
+        this.bodySprite,
+        this.frameX * this.spriteWidth,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
         this.x - this.bodyImage.width * 0.5 + 65,
-        this.y - this.bodyImage.width * 0.5 - 64
+        this.y - this.bodyImage.width * 0.5 - 64,
+        this.spriteWidth,
+        this.spriteHeight
       );
       // eye 1
-      context.beginPath();
-      context.arc(
-        this.x + Math.cos(this.angle) * this.radius * 0.35,
-        this.y + Math.sin(this.angle) * this.radius * 0.35,
-        this.radius * 0.6,
-        0,
-        Math.PI * 2
+      context.drawImage(
+        this.eye1Image,
+        this.x + Math.cos(this.angle) * this.eye1Radius - this.eye1Image.width * 0.5,
+        this.y + Math.sin(this.angle) * this.eye1Radius - this.eye1Image.height * 0.5
       );
-      context.stroke();
       // eye 2
-      context.beginPath();
-      context.arc(
-        this.x + Math.cos(this.angle) * this.radius * 0.6,
-        this.y + Math.sin(this.angle) * this.radius * 0.6,
-        this.radius * 0.3,
-        0,
-        Math.PI * 2
+      context.drawImage(
+        this.eye2Image,
+        this.x + Math.cos(this.angle) * this.eye2Radius - this.eye2Image.width * 0.5,
+        this.y + Math.sin(this.angle) * this.eye2Radius - this.eye2Image.height * 0.5
       );
-      context.fill();
+      // reflection
+      context.drawImage(
+        this.reflectionImage,
+        this.x - this.reflectionImage.width * 0.5,
+        this.y - this.reflectionImage.width * 0.5
+      );
+      // detectorLight
+      if (this.tracking) {
+        context.drawImage(
+          this.detectorLight,
+          this.x - this.detectorLight.width * 0.5,
+          this.y - this.detectorLight.width * 0.5 - 195
+        );
+      }
     }
     update() {
+      // angle
       const dx = this.mouse.x - this.x;
       const dy = this.mouse.y - this.y;
+      const distance = Math.hypot(dx, dy);
       this.angle = Math.atan2(dy, dx);
+
+      if (distance <= this.eye1Distance * 2.5) {
+        this.eye1Radius = distance * 0.4;
+        this.eye2Radius = distance * 0.65;
+      } else if (this.tracking) {
+        this.eye1Radius = this.eye1Distance;
+        this.eye2Radius = this.eye2Distance;
+      } else {
+        this.eye1Radius = this.eye1Distance * Math.cos(this.movementAngle);
+        this.eye2Radius = this.eye2Distance * Math.cos(this.movementAngle);
+      }
+      // sprite animation
+      this.frameX >= this.maxFrame ? (this.frameX = 0) : this.frameX++;
+      // movement
+      this.movementAngle += 0.005;
+      this.x = this.centerX + Math.cos(this.movementAngle * 3) * 20;
+      this.y = this.centerY + Math.sin(this.movementAngle * 0.5) * 50;
+      if (this.movementAngle > Math.PI * 4) {
+        this.movementAngle = 0;
+      }
     }
   }
   const robot = new Robot(canvas);
