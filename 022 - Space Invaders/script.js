c0675a5fa49344b33c7a026fdd12273b022df26d
SheriffKule <zeljko225@yahoo.com>
2025-11-08 16:36:23 +0100

    Space Invaders 1

---
+++ 022 - Space Invaders/script.js
@@ -0,0 +1,114 @@
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 100;
+    this.x = this.game.width * 0.5 - this.width * 0.5;
+    this.y = this.game.height - this.height;
+    this.speed = 10;
+  }
+  draw(context) {
+    context.lineWidth = 3;
+    context.strokeRect(this.x, this.y, this.width, this.height);
+    context.strokeRect(this.x + this.width / 4, this.y + this.height / 4, this.width / 2, this.height / 2);
+  }
+  update() {
+    // horizontal movement
+    if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+    if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+    // horizontal boundaries
+    if (this.x < 0) this.x = 0;
+    else if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+  }
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile) projectile.start(this.x, this.y);
+  }
+}
+
+class Projectile {
+  constructor() {
+    this.width = 4;
+    this.height = 20;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 20;
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+    }
+  }
+  start(x, y) {
+    this.x = x;
+    this.y = y;
+    this.free = false;
+  }
+  reset() {
+    this.free = true;
+  }
+}
+
+class Enemy {}
+
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.keys = [];
+    this.player = new Player(this);
+
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
+    console.log(this.projectilesPool);
+
+    //event listeners
+    window.addEventListener('keydown', (e) => {
+      if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+    });
+    window.addEventListener('keyup', (e) => {
+      const index = this.keys.indexOf(e.key);
+      if (index > -1) this.keys.splice(index, 1);
+    });
+  }
+  render(context) {
+    this.player.draw(context);
+    this.player.update();
+  }
+  // create projectiles object pool
+  createProjectiles() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+  // get free projectile object from the pool
+  getProjectile() {
+    for (let i = 0; i < this.projectilesPool; i++) {
+      if (this.projectilesPool[i].free) return this.projectilesPool[i];
+    }
+  }
+}
+
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 800;
+
+  const game = new Game(canvas);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
