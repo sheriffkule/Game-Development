ced286c257afb01fc9ef2f055023438574e021b5
SheriffKule <zeljko225@yahoo.com>
2025-11-12 23:15:33 +0100

    Planet Defense 2

--- 023 - Planet Defense/script.js
+++ 023 - Planet Defense/script.js
@@ -8,9 +8,11 @@ class Planet {
   }
   draw(context) {
     context.drawImage(this.image, this.x - 100, this.y - 100);
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.stroke();
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      context.stroke();
+    }
   }
 }
 
@@ -21,14 +23,80 @@ class Player {
     this.y = this.game.height * 0.5;
     this.radius = 40;
     this.image = document.getElementById('player');
+    this.aim;
+    this.angle = 0;
   }
   draw(context) {
-    context.drawImage(this.image, this.x - this.radius, this.y - this.radius);
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.stroke();
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(this.image, -this.radius, -this.radius);
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(0, 0, this.radius, 0, Math.PI * 2);
+      context.stroke();
+    }
+    context.restore();
   }
-  update() {}
+  update() {
+    this.aim = this.game.calcAim(this.game.planet, this.game.mouse);
+    this.x = this.game.planet.x + (this.game.planet.radius + this.radius) * this.aim[0];
+    this.y = this.game.planet.y + (this.game.planet.radius + this.radius) * this.aim[1];
+    this.angle = Math.atan2(this.aim[3], this.aim[2]);
+  }
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile)
+      projectile.start(
+        this.x + this.radius * this.aim[0],
+        this.y + this.radius * this.aim[1],
+        this.aim[0],
+        this.aim[1]
+      );
+  }
+}
+
+class Projectile {
+  constructor(game) {
+    this.game = game;
+    this.x;
+    this.y;
+    this.radius = 20;
+    this.speedX = 1;
+    this.speedY = 1;
+    this.speedModifier = 5;
+    this.free = true;
+  }
+  start(x, y, speedX, speedY) {
+    this.free = false;
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX * this.speedModifier;
+    this.speedY = speedY * this.speedModifier;
+  }
+  reset() {
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.save();
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      context.strokeStyle = 'gold';
+      context.stroke();
+      context.restore();
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+    // reset if outside the visible game area
+    if (this.x < 0 || this.x > this.game.width || this.y < 0 || this.y > this.game.height) {
+      this.reset();
+    }
+  }
 }
 
 class Game {
@@ -38,6 +106,11 @@ class Game {
     this.height = this.canvas.height;
     this.planet = new Planet(this);
     this.player = new Player(this);
+    this.debug = true;
+
+    this.projectilePool = [];
+    this.numberOfProjectiles = 30;
+    this.createProjectilePool();
 
     this.mouse = {
       x: 0,
@@ -49,23 +122,43 @@ class Game {
       this.mouse.x = e.offsetX;
       this.mouse.y = e.offsetY;
     });
+
+    window.addEventListener('mousedown', (e) => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+      this.player.shoot();
+    });
+
+    window.addEventListener('keyup', (e) => {
+      if (e.key === 'd') this.debug = !this.debug;
+    });
   }
   render(context) {
     this.planet.draw(context);
     this.player.draw(context);
     this.player.update();
-    context.beginPath();
-    context.moveTo(this.planet.x, this.planet.y);
-    context.lineTo(this.mouse.x, this.mouse.y);
-    context.stroke();
+    this.projectilePool.forEach((projectile) => {
+      projectile.draw(context);
+      projectile.update();
+    });
   }
   calcAim(a, b) {
     const dx = a.x - b.x;
     const dy = a.y - b.y;
     const distance = Math.hypot(dx, dy);
-    const aimX = dx / distance;
-    const aimY = dy / distance;
+    const aimX = (dx / distance) * -1;
+    const aimY = (dy / distance) * -1;
     return [aimX, aimY, dx, dy];
+  }
+  createProjectilePool() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilePool.push(new Projectile(this));
+    }
+  }
+  getProjectile() {
+    for (let i = 0; i < this.projectilePool.length; i++) {
+      if (this.projectilePool[i].free) return this.projectilePool[i];
+    }
   }
 }
 
