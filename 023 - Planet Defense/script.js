9606b330264f4cb892f1416bd6b3663eecafd030
SheriffKule <zeljko225@yahoo.com>
2025-11-14 21:12:35 +0100

    Planet Defense 3

--- 023 - Planet Defense/script.js
+++ 023 - Planet Defense/script.js
@@ -61,7 +61,7 @@ class Projectile {
     this.game = game;
     this.x;
     this.y;
-    this.radius = 20;
+    this.radius = 5;
     this.speedX = 1;
     this.speedY = 1;
     this.speedModifier = 5;
@@ -99,6 +99,95 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(game) {
+    this.game = game;
+    this.x = 100;
+    this.y = 100;
+    this.radius = 40;
+    this.width = this.radius * 2;
+    this.height = this.radius * 2;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.free = true;
+  }
+  start() {
+    this.free = false;
+    this.frameY = Math.floor(Math.random() * 4);
+    if (Math.random() < 0.5) {
+      this.x = Math.random() * this.game.width;
+      this.y = Math.random() < 0.5 ? -this.radius : this.game.height + this.radius;
+    } else {
+      this.x = Math.random() < 0.5 ? -this.radius : this.game.width + this.radius;
+      this.y = Math.random() * this.game.height;
+    }
+    const aim = this.game.calcAim(this, this.game.planet);
+    this.speedX = aim[0];
+    this.speedY = aim[1];
+  }
+  reset() {
+    this.free = true;
+  }
+  hit(damage) {
+    this.lives -= damage;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x - this.radius,
+        this.y - this.radius,
+        this.width,
+        this.height
+      );
+      if (this.game.debug) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.stroke();
+      }
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      // check collision enemy / planet
+      if (this.game.checkCollision(this, this.game.planet)) {
+        this.reset();
+      }
+      // check collision enemy / planet
+      if (this.game.checkCollision(this, this.game.player)) {
+        this.reset();
+      }
+      // check collision enemy / projectiles
+      this.game.projectilePool.forEach((projectile) => {
+        if (!projectile.free && this.game.checkCollision(this, projectile)) {
+          projectile.reset();
+          this.hit(1);
+        }
+      });
+      if (this.lives < 1) this.frameX++;
+      if (this.frameX > this.maxFrame) this.reset()
+    }
+  }
+}
+
+class Asteroid extends Enemy {
+  constructor(game) {
+    super(game);
+    this.image = document.getElementById('asteroid');
+    this.frameX = 0;
+    this.frameY = Math.floor(Math.random() * 4);
+    this.maxFrame = 7;
+    this.lives = 5;
+    this.maxLives = this.lives;
+  }
+}
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
@@ -109,8 +198,15 @@ class Game {
     this.debug = true;
 
     this.projectilePool = [];
-    this.numberOfProjectiles = 30;
+    this.numberOfProjectiles = 20;
     this.createProjectilePool();
+
+    this.enemyPool = [];
+    this.numberOfEnemies = 20;
+    this.createEnemyPool();
+    this.enemyPool[0].start();
+    this.enemyTimer = 0;
+    this.enemyInterval = 1700;
 
     this.mouse = {
       x: 0,
@@ -131,9 +227,10 @@ class Game {
 
     window.addEventListener('keyup', (e) => {
       if (e.key === 'd') this.debug = !this.debug;
+      else if (e.key === '1') this.player.shoot();
     });
   }
-  render(context) {
+  render(context, deltaTime) {
     this.planet.draw(context);
     this.player.draw(context);
     this.player.update();
@@ -141,6 +238,18 @@ class Game {
       projectile.draw(context);
       projectile.update();
     });
+    this.enemyPool.forEach((enemy) => {
+      enemy.draw(context);
+      enemy.update();
+    });
+    // periodically activate an enemy
+    if (this.enemyTimer < this.enemyInterval) {
+      this.enemyTimer += deltaTime;
+    } else {
+      this.enemyTimer = 0;
+      const enemy = this.getEnemy();
+      if (enemy) enemy.start();
+    }
   }
   calcAim(a, b) {
     const dx = a.x - b.x;
@@ -150,6 +259,13 @@ class Game {
     const aimY = (dy / distance) * -1;
     return [aimX, aimY, dx, dy];
   }
+  checkCollision(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const sumOfRadii = a.radius + b.radius;
+    return distance < sumOfRadii;
+  }
   createProjectilePool() {
     for (let i = 0; i < this.numberOfProjectiles; i++) {
       this.projectilePool.push(new Projectile(this));
@@ -158,6 +274,16 @@ class Game {
   getProjectile() {
     for (let i = 0; i < this.projectilePool.length; i++) {
       if (this.projectilePool[i].free) return this.projectilePool[i];
+    }
+  }
+  createEnemyPool() {
+    for (let i = 0; i < this.numberOfEnemies; i++) {
+      this.enemyPool.push(new Asteroid(this));
+    }
+  }
+  getEnemy() {
+    for (let i = 0; i < this.enemyPool.length; i++) {
+      if (this.enemyPool[i].free) return this.enemyPool[i];
     }
   }
 }
@@ -171,10 +297,13 @@ window.addEventListener('load', function () {
   ctx.lineWidth = 2;
 
   const game = new Game(canvas);
+  let lastTime = 0;
 
-  function animate() {
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.render(ctx);
+    game.render(ctx, deltaTime);
     requestAnimationFrame(animate);
   }
   animate();
