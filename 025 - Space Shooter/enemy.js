7986e00d57e425587a62be912766c13a2dc17ce7
SheriffKule <zeljko225@yahoo.com>
2025-11-16 18:45:15 +0100

    Space Shooter 2

---
+++ 025 - Space Shooter/enemy.js
@@ -0,0 +1,36 @@
+class Enemy {
+  constructor(game) {
+    this.game = game;
+    this.width = 50;
+    this.height = 50;
+    this.x = Math.random() * this.game.width;
+    this.y = -this.height;
+    this.speedX = 0;
+    this.speedY = Math.random() * 4 + 1;
+    this.free = true;
+  }
+  start() {
+    this.free = false;
+  }
+  reset() {
+    this.x = Math.random() * this.game.width;
+    this.y = -this.height;
+    this.free = true;
+  }
+  update() {
+    if (!this.free) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+
+      if (this.y > this.game.height) {
+        this.reset();
+      }
+    }
+  }
+  draw() {
+    if (!this.free) {
+      this.game.ctx.fillStyle = 'orangered';
+      this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
--- 025 - Space Shooter/main.js
+++ 025 - Space Shooter/main.js
@@ -1,10 +1,77 @@
-class Enemy {}
+class Game {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
 
-class Game {}
+    this.enemyPool = [];
+    this.numberOfEnemies = 50;
+    this.createEnemyPool();
+    this.enemyTimer = 0;
+    this.enemyInterval = 1000;
+
+    this.start();
+
+    window.addEventListener('resize', (e) => {
+      this.resize(e.target.innerWidth, e.target.innerHeight);
+    });
+  }
+  start() {
+    this.resize(window.innerWidth, window.innerHeight);
+  }
+  resize(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.width = width;
+    this.height = height;
+    this.ctx.fillStyle = 'seagreen';
+  }
+  createEnemyPool() {
+    for (let i = 0; i < this.numberOfEnemies; i++) {
+      this.enemyPool.push(new Enemy(this));
+    }
+  }
+  getEnemy() {
+    for (let i = 0; i < this.enemyPool.length; i++) {
+      if (this.enemyPool[i].free) return this.enemyPool[i];
+    }
+  }
+  handleEnemies(deltaTime) {
+    if (this.enemyTimer < this.enemyInterval) {
+      this.enemyTimer += deltaTime;
+    } else {
+      this.enemyTimer = 0;
+      const enemy = this.getEnemy();
+      if (enemy) enemy.start();
+    }
+  }
+  render(deltaTime) {
+    this.handleEnemies(deltaTime);
+    this.enemyPool.forEach((enemy) => {
+      enemy.update();
+      enemy.draw();
+    });
+  }
+}
 
 window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  ctx.fillStyle = 'seagreen';
+
+  const game = new Game(canvas, ctx);
+
+  let lastTime = 0;
+
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(deltaTime);
+    requestAnimationFrame(animate);
+  }
+  animate(0);
 });
