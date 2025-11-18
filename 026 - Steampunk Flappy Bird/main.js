ee7aec253311ea896fee06eb6504fe115531eeac
SheriffKule <zeljko225@yahoo.com>
2025-11-18 15:42:12 +0100

    Steampunk Flappy Bird 2

--- 026 - Steampunk Flappy Bird/main.js
+++ 026 - Steampunk Flappy Bird/main.js
@@ -6,13 +6,29 @@ class Game {
     this.height = this.canvas.height;
     this.baseHeight = 720;
     this.ratio = this.height / this.baseHeight;
+    this.background = new Background(this);
     this.player = new Player(this);
+    this.obstacles = [];
+    this.numberOfObstacles = 10;
     this.gravity;
+    this.speed;
 
     this.resize(window.innerWidth, window.innerHeight);
 
     window.addEventListener('resize', (e) => {
       this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+    });
+    //mouse controls
+    this.canvas.addEventListener('mousedown', (e) => {
+      this.player.flap();
+    });
+    // keyboard controls
+    window.addEventListener('keydown', (e) => {
+      if (e.key === ' ' || e.key === 'Enter') this.player.flap();
+    });
+    // touch controls
+    this.canvas.addEventListener('touchstart', (e) => {
+      this.player.flap();
     });
   }
   resize(width, height) {
@@ -24,11 +40,23 @@ class Game {
     this.ratio = this.height / this.baseHeight;
 
     this.gravity = 0.15 * this.ratio;
+    this.speed = 2 * this.ratio;
+    this.background.resize();
     this.player.resize();
   }
   render() {
+    this.background.update();
+    this.background.draw();
     this.player.update();
     this.player.draw();
+  }
+  createObstacles() {
+    this.obstacles = [];
+    const firstX = 100;
+    const obstacleSpacing = 100;
+    for (let i = 0; i < this.numberOfObstacles; i++) {
+      this.obstacles.push(new Obstacles(this, firstX + i * obstacleSpacing));
+    }
   }
 }
 
--- 026 - Steampunk Flappy Bird/player.js
+++ 026 - Steampunk Flappy Bird/player.js
@@ -8,6 +8,7 @@ class Player {
     this.width;
     this.height;
     this.speedY;
+    this.flapSpeed;
   }
   draw() {
     this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -27,8 +28,17 @@ class Player {
     this.height = this.spriteHeight * this.game.ratio;
     this.y = this.game.height * 0.5 - this.height * 0.5;
     this.speedY = -5 * this.game.ratio;
+    this.flapSpeed = 5 * this.game.ratio;
+  }
+  isTouchingTop() {
+    return this.y <= 0;
   }
   isTouching() {
-    return this.y >= this.game.height - this.height
+    return this.y >= this.game.height - this.height;
+  }
+  flap() {
+    if (!this.isTouchingTop()) {
+      this.speedY = -this.flapSpeed;
+    }
   }
 }
