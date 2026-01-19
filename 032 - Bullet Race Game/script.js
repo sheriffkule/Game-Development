48ec8395fcf268e9841a8dfe39e56bbb3a8bbbf0
SheriffKule <zeljko225@yahoo.com>
2026-01-19 14:00:58 +0100

    Bullet Race Game 2

--- 032 - Bullet Race Game/script.js
+++ 032 - Bullet Race Game/script.js
@@ -107,3 +107,130 @@ function ObstacleManager(lanes, car) {
 
   this._init();
 }
+
+function CollisionHandler(car, obstacleManager) {
+  let _this = this;
+  this._init = function () {
+    this.car = car;
+    this.obstacleManager = obstacleManager;
+  };
+
+  this.checkCollisions = function () {
+    for (let i = 0; i < _this.obstacleManager.obstacles.length; i++) {
+      return _this._checkCollision(_this.obstacleManager.obstacles[i]);
+    }
+  };
+
+  this._checkCollision = function (obstacle0) {
+    if (
+      obstacle0.dynamicMarginTop + OBSTACLE_HEIGHT > NFS_HEIGHT - CAR_HEIGHT &&
+      obstacle0.laneNo === _this.car.currentLane
+    ) {
+      return true;
+    }
+  };
+
+  this.checkShot = function () {
+    let bulletLane;
+
+    for (let i = 0; i < _this.obstacleManager.lanes.length; i++) {
+      if (_this.obstacleManager.lanes[i].getElementByClassName('bullets').length > 0) {
+        bulletLane = i;
+        break;
+      }
+    }
+
+    let oldestObstacleInCurrentLane = obstacleManager.getOldestObstacleInLane(bulletLane);
+    if (oldestObstacleInCurrentLane !== undefined && _this.car.gun.bullets !== undefined) {
+      if (
+        _this.car.gun.dynamicBulletBottom +
+          oldestObstacleInCurrentLane.dynamicMarginTop +
+          CAR_HEIGHT +
+          OBSTACLE_HEIGHT >
+        NFS_HEIGHT
+      ) {
+        _this.car.gun.stop();
+        _this.car.enableGun();
+        _this.obstacleManager.removeObstacle(oldestObstacleInCurrentLane);
+      }
+    }
+    if (_this.car.gun.dynamicBulletBottom + CAR_HEIGHT > NFS_HEIGHT) {
+      _this.car.gun.stop();
+      _this.car.enableGun();
+    }
+  };
+
+  this._init();
+}
+
+function Car(currentLane, lanes) {
+  let _this = this;
+  this._init = function () {
+    this.currentLane = currentLane;
+    this.lanes = lanes;
+    this.element = document.createElement('img');
+    this.element.className = 'car';
+    this.element.setAttribute('src', 'images/car.png');
+    this.lanes[this.currentLane].appendChild(this.element);
+    this.gun = new Gun(this.lanes);
+  };
+
+  this._changeLane = function (laneNumber) {
+    if (_this.currentLane !== laneNumber) {
+      _this.lanes[laneNumber].appendChild(_this.element);
+    }
+    if (_this.currentLane > laneNumber) {
+      _this.currentLane--;
+    } else if (_this.currentLane < laneNumber) {
+      _this.currentLane++;
+    }
+  };
+
+  this._keyNavigation = function (e) {
+    switch (e.which) {
+      case 37:
+        _this._changeLane(_this.currentLane - 1 < 0 ? 0 : _this.currentLane - 1);
+        break;
+      case 39:
+        _this._changeLane(
+          _this.currentLane + 1 > _this.lanes.length - 1 ? _this.lanes.length - 1 : _this.currentLane + 1,
+        );
+        break;
+      default:
+        return;
+    }
+  };
+
+  this._fireGunEvent = function (e) {
+    switch (e.which) {
+      case 32:
+        _this._disableGun();
+        _this.gun.fireBullets(_this.currentLane);
+        break;
+    }
+  };
+
+  this.enableGun = function () {
+    document.addEventListener('keyup', this._fireGunEvent, false);
+  };
+
+  this._disableGun = function () {
+    document.removeEventListener('keyup', this._fireGunEvent, false);
+  };
+
+  this._initEvents = function () {
+    document.addEventListener('keydown', this._keyNavigation, false);
+    this.enableGun();
+  };
+
+  this._removeEvents = function () {
+    document.removeEventListener('keydown', this._keyNavigation, false);
+    this._disableGun();
+  };
+
+  this.stop = function () {
+    _this._removeEvents();
+  };
+
+  this._init();
+}
