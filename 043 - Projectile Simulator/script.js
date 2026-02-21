a8c7ec75d5563eee4313be62e4fa98d0d8f0fa7c
SheriffKule <zeljko225@yahoo.com>
2026-02-21 11:40:39 +0100

    Projectile Simulator 1

---
+++ 043 - Projectile Simulator/script.js
@@ -0,0 +1,15 @@
+class Projectile {
+  element;
+  x;
+  y;
+  g;
+  horizontalRange;
+  maxHeight;
+  actualHorizontalRange;
+
+  constructor(speed) {
+    this.element = this.element;
+    this.x = x;
+    this.y = y;
+  }
+}
