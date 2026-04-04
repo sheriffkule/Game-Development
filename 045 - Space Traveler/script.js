54950348e0ec18454628b37c83a1f5203598c7f3
SheriffKule <zeljko225@yahoo.com>
2026-04-04 15:20:13 +0200

    Space Traveler 5

--- 045 - Space Traveler/script.js
+++ 045 - Space Traveler/script.js
@@ -44,7 +44,7 @@ let Ballon = function (arg) {
 
   let obj = function (base) {
     let th = {};
-    lt.age = 0;
+    th.age = 0;
     th.size = base.size || D;
     th.x = base.x || 0;
     th.y = base.y || 0;
@@ -63,7 +63,7 @@ let Ballon = function (arg) {
   };
 
   // Backbround stars
-  let bg1 = function () {
+  let bg1 = function (base) {
     let th = obj(base);
     th.v = sv * (0.3 + Math.random() * 0.5);
     th.size = 2;
@@ -116,7 +116,7 @@ let Ballon = function (arg) {
     th.active = 1;
     th.tick = function () {
       th.age++;
-      th.tick();
+      th.tick0();
       return th.age;
     };
     th.banged = function () {
@@ -149,26 +149,28 @@ let Ballon = function (arg) {
       if (th.checkifdead() >= 3) return th.age;
       th.age++;
       if (th.checkifdead() == 0) th.lived++;
-      th.vy += 0.2;
-      if (th.vy > 2.8) th.vy = 2.8;
-      if (in_f > 0) {
-        th.vx += (in_r - in_l) * (th.acc > 4 ? 0.2 : 0.24);
-        if (th.vx > 2.8) th.vx = 2.8;
-        if (th.vx < -2.8) th.vx = -2.8;
+      if (state == 1) {
+        th.vy += 0.2;
+        if (th.vy > 2.8) th.vy = 2.8;
+        if (in_f > 0) {
+          th.vx += (in_r - in_l) * (th.acc > 4 ? 0.2 : 0.24);
+          if (th.vx > 2.8) th.vx = 2.8;
+          if (th.vx < -2.8) th.vx = -2.8;
+        }
+        if (th.acc < 4) {
+          th.vy -= 0.4;
+        } else if (th.acc < 8 || in_f == 0) {
+          th.vy -= 0.32;
+        }
+        if (th.vy < -2.8) th.vy = -2.8;
+        th.x += th.vx;
+        th.y += th.vy;
+        if (th.x <= th.size / 2) th.x = th.size / 2;
+        if (th.y <= th.size / 2) th.y = th.size / 2;
+        if (th.x > w - th.size / 2) th.x = w - th.size / 2;
+        if (th.y > h - th.size / 2) th.y = h - th.size / 2;
+        th.acc++;
       }
-      if (th.acc < 4) {
-        th.vy -= 0.4;
-      } else if (th.acc < 8 || in_f == 0) {
-        th.vy -= 0.32;
-      }
-      if (th.vy < -2.8) th.vy = -2.8;
-      th.x += th.vx;
-      th.y += th.vy;
-      if (th.x <= th.size / 2) th.x = th.size / 2;
-      if (th.y <= th.size / 2) th.y = th.size / 2;
-      if (th.x > w - th.size / 2) th.x = w - th.size / 2;
-      if (th.x > h - th.size / 2) th.y = h - th.size / 2;
-      th.acc++;
       return th.age;
     };
     th.checkifdead = function () {
@@ -194,7 +196,7 @@ let Ballon = function (arg) {
     bal = [];
     pt = [];
     score = bal_cnt = bal_cmb = bal_max = spcnt = 0;
-    bas_rank = 1;
+    bal_rank = 1;
     randflag = false;
     level = 1;
     p = player({ x: w / 2, y: h / 2, vy: -3 });
@@ -423,5 +425,89 @@ let Ballon = function (arg) {
       );
       ctx[1].restore();
     }
+    for (let i = 0; i < water.length; i++) {
+      ctx[1].drawImage(
+        sp[0],
+        5 * D,
+        0,
+        water[i].size,
+        water[i].size,
+        water[i].x - water[i].size / 2,
+        water[i].y - water[i].size / 2,
+        water[i].size,
+        water[i].size,
+      );
+    }
+    ctx[1].fillStyle = 'yellow';
+    for (let i = 0; i < pt.length; i++) {
+      ctx[1].fillText(pt[i].text, pt[i].x - D, pt[i].y);
+    }
+    if (state == 0 || state == 3) ctx[1].globalAlpha = 1;
+    else ctx[1].globalAlpha = 1;
+    if (state == 3) {
+      ctx[1].fillText('GAME OVER', 200, h / 2 - 15);
+      ctx[1].fillText('PLEASE TRY AGAIN!', 185, h / 2 + 15);
+    } else if (state == 0) {
+      ctx[1].fillText('SPACE TRAVEL', 200, h / 2 - 15);
+      ctx[1].fillText('Press Z KEY TO START NEW GAME', 140, h / 2 + 15);
+    }
+    ctx[1].fillStyle = '#ffffff';
+    ctx[1].fillText('SCORE', 5, 15);
+    ctx[1].fillText(score, 80, 15);
+    ctx[1].fillText('BALLOON', 5, 28);
+    ctx[1].fillText(bal_cnt, 80, 28);
+    ctx[1].fillText('MAX-COMBO ' + bal_max, w - 110, 15);
+    ctx[0].putImageData(ctx[1].getImageData(0, 0, w, h), 0, 0);
   };
+
+  this.kd = function (e) {
+    mv(e.keyCode, 1);
+  };
+
+  this.ky = function (e) {
+    mv(e.keyCode, 0);
+  };
+
+  let mv = function (cd, val) {
+    if (cd == 37) {
+      in_l = val;
+      p.forward = 1;
+    } else if (cd == 39) {
+      in_r = val;
+      p.forward = 0;
+    } else if (cd == 90) {
+      in_f = val;
+      if (state == 0 && val == 1) {
+        state = 1;
+        t = t0 = 0;
+        init0();
+      } else if (state == 1) {
+        if (val == 1) p.acc = 0;
+      } else if (state == 3 && val == 1) {
+        state = 0;
+      }
+    }
+  };
+
+  var dcd = function (arr) {
+    let in1 = arr.split('Z');
+    let ret = [];
+    for (let i = 0; i < in1.length; i++) {
+      let str = '';
+      let in2 = in1[i].split('');
+      for (let j = 0; j < in2.length; j++) {
+        let a = in2[j];
+        str += a;
+        if (in2[j + 1] < '0' || in2[j + 1] > '9') continue;
+        let n = in2[++j];
+        if (in2[j + 1] >= '0' && in2[j + 1] <= '9') n += in2[++j];
+        for (let k = 0; k < n - 1; k++) str += a;
+      }
+      ret.push(str)
+    }
+    return ret;
+  };
+  // bg
+  let lighttable = []
+  let lighttable0 = "S3LS3BS7LS4W2ZS2LSLS9LSLS3W2ZS3LS11LS4W2ZS20W2ZS9LS7BS2W2ZLS3BS3LSLS9W2ZSLS7LS10W2ZS2LS13LS3W2ZS2LS12LS4W2ZS3L2S6BS2LS5W2ZS6LS7LS5W2ZS9BS3LS6W2ZS2BS10LS6W2ZS20W2ZSLS7LS7LS2W2ZLSLS5LSLS5LSLSW2ZSLS7LS7LS2W2ZS5BS14W2ZS20W2ZS4LS7LS3BS3W2ZS3LSLS5LSLS6W2ZS4LS7LS7W2ZS17LS2W2ZS16LSLSW2ZS5BS2LS3BS4LS2W2ZSLS18W2ZLSLS17W2ZSLS10LS7W2ZS6LS4LSLS6W2ZS5LSLS4LS7W2ZBS5LS11LSW2ZS14BS2LSLW2ZS6BS11LSW2ZS11BS8W2ZS20W2ZS5LS5LS8W2ZS5LS6LS7W2ZS4LS6BS8W2ZS2L2S12LS3W2ZS20W2ZLS9LS9W2ZS6LS13W2ZS14LS5W2ZS2BS5BS4LSLS4W2ZS20W2ZS2LS17W2ZS6LS13W2ZS7LS7LS4W2ZS7LS5BS2LS3W2ZSLS6LS3BS3LS3W2ZS2LS5LS6LS4W2ZS3LS4LS5LS5W2ZS9LS8LSW2ZSBS18W2ZS8BS6BS4W2ZLS5LS10LS2W2ZS5LS5LS6LSW2ZS19LW2ZS4LS15W2ZS19LW2ZLSLS5LS11W2ZSLS15LS2W2ZS10BS9W2ZS7LS7BS4W2ZSBS18W2ZS13LS6W2ZBS2LS16W2ZS8LS4LS6W2ZS20W2ZSBS13LS4W2ZS6LS13W2ZLS16BS2W2ZS3BS5LS10W2ZS10LS8LW2ZS3LS16W2ZS20W2ZS19LW2ZS17BSLW2ZS19LW2ZS17BSLW2ZS19LW2ZS17BSLW2ZS19LW2ZS17BSLW2ZS19LW2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS10LS9W2ZS8BSLSBS7W2ZS10LS9W2ZS8BSLSBS7W2ZS10LS9W2ZS8BSLSBS7W2ZS10LS9W2ZS8BSLSBS7W2ZS10LS9W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZLS19W2ZLSBS17W2ZLS19W2ZLSBS17W2ZLS19W2ZLSBS17W2ZLS19W2ZLS19W2ZLBS18W2ZLBS18W2ZS20W2ZS20W2ZS20W2ZS20W2ZLS17LSW2ZSLS13BSLS2W2ZSLS13BSLS2W2ZS2LS13LS3W2ZS3LS12LS3W2ZS4LS10LS4W2ZS4LS8L2S5W2ZS5LS6LS7W2ZS6LS4LS8W2ZSLS14LS3W2ZS2LS12LS4W2ZS2LS11LS5W2ZS3L2S7L2S3BSLW2ZSBS4LS3L2S4BS2LW2ZS2BS4L3S5BS3LW2ZLS2BS14LSW2ZSLS15LS2W2ZS2LS13LS3W2ZS3L2S11LS3W2ZS5LSLSLS4LS5W2ZS8LS4LS6W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS6BS2LS2BS7W2ZS5BS2LSLS2BS6W2ZS4BS2LSLSLS2BS5W2ZS3BS2LSLSLSLS2BS4W2ZS5LSLSLSLSLS6W2ZS4LSLSLSLSLSLS5W2ZS4LSLSLSLSLSLS5W2ZS5LSLSLSLSLS6W2ZS3BS2LSLSLSLS2BS4W2ZS4BS2LSLSLS2BS5W2ZS5BS2LSLS2BS6W2ZS6BS2LS2BS7W2ZS20W2ZS20W2ZS20W2ZLS19W2ZS20W2ZS2LS5LS11W2ZS15LS4W2ZS20W2ZLS9LS9W2ZS4LS15W2ZS17LS2W2ZS7BS12W2ZS12LS7W2ZS2LS17W2ZS6LS13W2ZS20W2ZS2BS11LS2BS2W2ZLS19W2ZS9LS10W2ZS17LS2W2ZS4LS15W2ZS13LS6W2ZS20W2ZLS19W2ZS20W2ZS15LS4W2ZS9LS10W2ZS4LS15W2ZS20W2ZS2BS13BS3W2ZLS12LS4LSW2ZS2BS5LS8BS2W2ZS20W2ZSBS16BSW2ZS3LS16W2ZS15LS4W2ZS10LS9W2ZS20W2ZS20W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZLS2BS13BS2W2ZS20W2ZLS19W2ZS3BS13BS2W2ZLS19W2ZS20W2ZS3BS13BS2W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2ZS20W2";
 };
